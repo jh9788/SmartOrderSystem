c8a337079a2aa87df783ee197c407ae0f83413eb
jh9788 <gkwhdgns2@gmail.com>
2023-05-07 18:55:03 +0900

    first

---
+++ src/main/java/com/SOS/SmartOrderSystem/SmartOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.SOS.SmartOrderSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SOS/SmartOrderSystem/SmartOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SOS.SmartOrderSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
