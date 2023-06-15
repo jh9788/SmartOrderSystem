51f547750874af58f0c969c43102cf93b0f8be40
jh9788 <gkwhdgns2@gmail.com>
2023-06-15 23:42:55 +0900

    0.13

--- src/main/java/com/SOS/SmartOrderSystem/domain/Owner.java
+++ src/main/java/com/SOS/SmartOrderSystem/domain/Owner.java
@@ -2,16 +2,66 @@ package com.SOS.SmartOrderSystem.domain;
 
 public class Owner {
     private String id;
+    private String password;
     private String name;
     private String sex;
     private String email;
     private String phoneNumber;
 
-    public Owner(String id, String name, String sex, String email, String phoneNumber) {
+    public Owner(String id, String password, String name, String sex, String email, String phoneNumber) {
         this.id = id;
+        this.password = password;
         this.name = name;
         this.sex = sex;
         this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 }
