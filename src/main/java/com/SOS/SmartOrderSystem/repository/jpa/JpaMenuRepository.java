218405b1bb3897f58a5c77f4215470ab250380a4
jh9788 <gkwhdgns2@gmail.com>
2023-09-24 16:52:35 +0900

    0.42

--- src/main/java/com/SOS/SmartOrderSystem/repository/jpa/JpaMenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SOS.SmartOrderSystem.repository.jpa;
-
-import com.SOS.SmartOrderSystem.domain.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaMenuRepository extends JpaRepository<Menu, Long> {
-}
--- src/main/java/com/SOS/SmartOrderSystem/repository/jpa/JpaOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SOS.SmartOrderSystem.repository.jpa;
-
-import com.SOS.SmartOrderSystem.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaOrderRepository extends JpaRepository<Order, Long> {
-
-}
